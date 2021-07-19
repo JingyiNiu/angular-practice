8d1535cb945d68555d2064021108979484a0d04f
JingyiNiu <niu.jy1203@gmail.com>
2021-07-20 11:47:39 +1200

    first commit and create basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainContainerComponent } from './components/main-container/main-container.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PostsComponent } from './pages/posts/posts.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    MainContainerComponent,
+    HomeComponent,
+    PostsComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
